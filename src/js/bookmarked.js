1ebd9b585d5089b9cb64ae7a43b0007dcd4c6421
pierrerosier <contact@pierrerosier.fr>
2021-05-17 15:27:49 +0200

    modify js and img address (again…)

--- src/js/bookmarked.js
+++ src/js/bookmarked.js
@@ -1,6 +1,11 @@
 const btnBooked = document.querySelector('#bookmarked');
+const goBack = document.querySelector('.btn__back')
 const elt = document.querySelector('.is-grey')
 
 btnBooked.addEventListener('click', ()=> {
     elt.classList.add('is-success')
+})
+
+goBack.addEventListener('click', ()=> {
+    elt.classList.remove('is-success')
 })
